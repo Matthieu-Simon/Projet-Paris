39d07013d27ad562d62fd49df5eb4396798eb97d
Matthieu-Simon <simon.matthieu89@gmail.com>
2024-03-20 15:09:20 +0100

    Ajout Question Pie chart

--- main_file.py
+++ main_file.py
@@ -18,16 +18,15 @@ import save_data_from_api as sd
 
 #import gaussian_kde
 "%matplotlib inline"
-if sd.creation_date_today()==False:
+if sd.creation_date_today() == False:
     sd.save_data_from_online()
 
 
-
-df=pandas.read_csv(dossier_execution + "/sanisettes_paris.csv",sep=';',header=0)
+df = pandas.read_csv(dossier_execution + "/sanisettes_paris.csv",sep=';',header=0)
 df = df.drop('URL_FICHE_EQUIPEMENT', axis=1)
 df = df.drop('geo_shape', axis=1)
 df = df.drop('geo_point_2d', axis=1)
-df=df.drop('STATUT', axis=1)
+df = df.drop('STATUT', axis=1)
 
 def modifier_valeur(valeur):
     if pandas.isna(valeur):
@@ -49,7 +48,7 @@ def split_arr(valeur):
 
 df['ACCES_PMR'] = df['ACCES_PMR'].apply(modifier_valeur)
 df['RELAIS_BEBE'] = df['RELAIS_BEBE'].apply(modifier_valeur)
-df['ARRONDISSEMENT']=df['ARRONDISSEMENT'].apply(split_arr)
-df['HORAIRE']=df['HORAIRE'].apply(modifier_horaire)
+df['ARRONDISSEMENT'] = df['ARRONDISSEMENT'].apply(split_arr)
+df['HORAIRE'] = df['HORAIRE'].apply(modifier_horaire)
 print(df)
 print(df.shape)
